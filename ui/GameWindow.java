fddd25de14973fc17424a37cbb0df3a586596aa9
Terry <terry95193@gmail.com>
2021-02-22 03:27:54 +0800

    added cranes

--- ui/GameWindow.java
+++ ui/GameWindow.java
@@ -17,13 +17,13 @@ public class GameWindow {
 
         frame = new JFrame();
         panel = new JPanel();
-        panel.setLayout(new java.awt.GridLayout(5, 9));
+        panel.setLayout(new java.awt.GridLayout(5, 10));
         panel.setBackground(Color.BLACK);
 
         frame.add(panel);
         frame.setTitle("GAME TIME!");
         frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        frame.setSize(900, 500);
+        frame.setSize(1000, 500);
         drawBoard();
         frame.setVisible(true);
 
@@ -35,10 +35,20 @@ public class GameWindow {
 
     private void drawBoard() {
         ImageIcon imageIcon = new ImageIcon(new ImageIcon("./emptyIcon.png").getImage().
-                getScaledInstance(25, 25, Image.SCALE_DEFAULT));
+                getScaledInstance(50, 50, Image.SCALE_DEFAULT));
+        ImageIcon craneIcon = new ImageIcon(new ImageIcon("./crane.png").getImage().
+                getScaledInstance(50, 50, Image.SCALE_DEFAULT));
+        ImageIcon craneIconFlipped = new ImageIcon(new ImageIcon("./craneFlipped.png").getImage().
+                getScaledInstance(50, 50, Image.SCALE_DEFAULT));
 
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 5; j++) {
+//        Icon craneIcon = new ImageIcon("./crane.png");
+//        Icon craneIconFlipped = new ImageIcon("./craneFlipped.png");
+
+        for (int i = 0; i < 5; i++) {
+            final JLabel playerCraneLabel = new JLabel(craneIcon);
+            playerCraneLabel.setBorder(BorderFactory.createLineBorder(Color.WHITE));
+            panel.add(playerCraneLabel);
+            for (int j = 0; j <8; j++) {
                 final JLabel label = new JLabel(imageIcon);
                 String string = i + "" + j;
                 label.setName(string);
@@ -46,6 +56,9 @@ public class GameWindow {
                 setFontColour(label);
                 panel.add(label);
             }
+            final JLabel computerCraneLabel = new JLabel(craneIconFlipped);
+            computerCraneLabel.setBorder(BorderFactory.createLineBorder(Color.WHITE));
+            panel.add(computerCraneLabel);
         }
     }
 
