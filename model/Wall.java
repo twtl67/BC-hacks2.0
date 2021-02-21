87d66e7a04d22e4028fd6c8d596362fc53021e85
Terry <56202767+twtl67@users.noreply.github.com>
2021-02-22 04:52:23 +0800

    Update Wall.java
    copied from final-ver-submitted

--- model/Wall.java
+++ model/Wall.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:model/Wall.java
 package model;
 
 public class Wall {
@@ -18,23 +17,3 @@ public class Wall {
         return lives;
     }
 }
-=======
-package model;
-public class Wall {
-    // A wall has 3 lives at the beginning. It loses one life if a bot from the enemy team collides with it
-    private int lives;
-
-    public Wall() {
-        lives = 3;
-    }
-
-    public int getLives() {
-        return lives;
-    }
-
-    public int loseLife() {
-        lives--;
-        return lives;
-    }
-}
->>>>>>> e1ad5d8c3171bdaebca6d07549ae9b11ec4ba47c:Wall.java
